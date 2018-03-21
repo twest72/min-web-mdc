680f814db7f0b3480124634bd5cca1c1f52d436e
Thomas Westphal <twestphal4.ext@gema.de>
2018-03-21 02:38:46 +0100

    adds sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,45 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    // devtool: 'inline-source-map',
+    entry: {
+        index: './src/index.js',
+        css: './src/index.scss'
+    },
     devServer: {
         contentBase: './dist'
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
-            title: 'Webpack Starter'
+            title: 'Webpack Starter',
+            links: [{rel: 'stylesheet', type: 'text/css', href: '/index.css'}]
+        }),
+        new HtmlWebpackIncludeAssetsPlugin({
+            assets: ['index.css'],
+            append: false
         })
     ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'index.css'
+                    }
+                },
+                {loader: 'extract-loader'},
+                {loader: 'css-loader'},
+                {loader: 'sass-loader'}
+            ]
+        }]
     }
 };
